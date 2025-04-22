216244a84424656cf41e6e209380c5fbc2a87c6f
RROP <kimjiho1121@naver.com>
2025-04-22 17:29:48 +0900

    Git Action FIX TRY #18

--- SETcmake.cmake
+++ SETcmake.cmake
@@ -8,7 +8,7 @@ if(WIN32)
 
   set(CMAKE_CXX_STANDARD 20)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
-  set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+  # set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
   set(cmake_c_compiler "cl")
   set(cmake_cxx_compiler "cl")
 else()
