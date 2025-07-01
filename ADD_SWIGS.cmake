25709facecb712bb93b8607960ac3a3a5ac647b9
RROP <kimjiho1121@naver.com>
2025-07-01 20:58:45 +0900

    waiting for rocksdb is fixed.

--- ADD_SWIGS.cmake
+++ ADD_SWIGS.cmake
@@ -4,7 +4,7 @@ swig_add_library(pdje_csharp
   TYPE MODULE
   LANGUAGE CSharp
   OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/swig_csharp
-  SOURCES PDJE_swig.i ${miniaudio_src} ${SoundTouch_src} ${dbSource} ${audioRenderSource} ${sql_amalgam_src} ${editorSource}
+  SOURCES PDJE_swig.i ${audioRenderSource} ${SoundTouch_src} ${miniaudio_src} ${dbSource} ${sql_amalgam_src} ${editorSource}
 )
 find_package(Python REQUIRED COMPONENTS Interpreter Development)
 
@@ -12,14 +12,14 @@ swig_add_library(pdje_python
   TYPE MODULE
   LANGUAGE Python
   OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/swig_python
-  SOURCES PDJE_swig.i ${miniaudio_src} ${SoundTouch_src} ${dbSource} ${audioRenderSource} ${sql_amalgam_src} ${editorSource}
+  SOURCES PDJE_swig.i ${audioRenderSource} ${SoundTouch_src} ${miniaudio_src} ${dbSource} ${sql_amalgam_src} ${editorSource}
 )
 
 swig_add_library(pdje_go
   TYPE STATIC
   LANGUAGE go
   OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/swig_go
-  SOURCES PDJE_swig.i ${miniaudio_src} ${SoundTouch_src} ${dbSource} ${audioRenderSource} ${sql_amalgam_src} ${editorSource}
+  SOURCES PDJE_swig.i ${audioRenderSource} ${SoundTouch_src} ${miniaudio_src} ${dbSource} ${sql_amalgam_src} ${editorSource}
 )
 
 set_target_properties(${SWIG_MODULE_pdje_python_REAL_NAME} PROPERTIES
