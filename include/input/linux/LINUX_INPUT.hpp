b8273281990015eaf8d31902c39e729828340de9
RROP <kimjiho1121@naver.com>
2025-07-23 13:40:27 +0900

    Document input API and update docs instructions

--- include/input/linux/LINUX_INPUT.hpp
+++ include/input/linux/LINUX_INPUT.hpp
@@ -3,8 +3,18 @@
 #include <libevdev-1.0/libevdev/libevdev.h>
 
 
+/**
+ * @brief Linux implementation of the input backend using libevdev.
+ */
 class LinuxEVDEV{
 public:
+    /**
+     * @brief Enumerate available input devices on the system.
+     */
     void getList();
+
+    /**
+     * @brief Initialize the backend and prepare for input polling.
+     */
     void init();
 };
