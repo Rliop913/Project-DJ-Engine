c12b65044c11f8eb5dcc4d8c1e638676832b3238
Rliop913 <87229271+Rliop913@users.noreply.github.com>
2025-08-08 03:41:52 +0900

    fix flag error

--- BuildInitwithConan.sh
+++ BuildInitwithConan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-conan install $1 --output-folder=conan_cmakes --build=missing --settings compiler.cppstd=20 --settings build_type=$2
+conan install $1 --output-folder=conan_cmakes --build=missing --settings compiler.cppstd=20 --settings build_type=$2 --deployer=direct_deploy
