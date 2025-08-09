e040b4562022bb2fd894ad1193d816ba20fc6bc9
RROP <kimjiho1121@naver.com>
2025-08-10 04:56:25 +0900

    update debug flag

--- SETcmake.cmake
+++ SETcmake.cmake
@@ -55,7 +55,7 @@ endif()
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     
     if(NOT WIN32)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O1 -fsanitize=address")
+    # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O1 -fsanitize=address")
     endif()
     set_property(SOURCE PDJE_swig.i PROPERTY CPLUSPLUS ON)
 else()
