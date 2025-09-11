f23524293235c681b6f02917d64b6fef449fc7b1
RROP <kimjiho1121@naver.com>
2025-09-11 17:22:06 +0900

    organized directory

--- cmakes/src/AUDIOsrc.cmake
+++ cmakes/src/AUDIOsrc.cmake
@@ -1,36 +1,36 @@
 
 set(audioRenderSource
 
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixMachine.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MiniaudioObjects/Decoder.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MUSIC_CTR/MUSIC_CTR.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MUSIC_CTR/SetCue.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/EFFECT/EFFECTS.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixMachine.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MiniaudioObjects/Decoder.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MUSIC_CTR/MUSIC_CTR.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MUSIC_CTR/SetCue.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/EFFECT/EFFECTS.cpp
 
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/audioRender.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MUSIC_CTR/BattleDj.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_MUSCTR.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_COMPRESSOR.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_EQ.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/audioRender.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MUSIC_CTR/BattleDj.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_MUSCTR.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_COMPRESSOR.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_EQ.cpp
 
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_ECHO.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_FILTER.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_OCS_FILTER.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_PHASER.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_TRANCE.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_DISTORTION.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_FLANGER.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_PANNER.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_ROLL.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_FX_ROBOT.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/MixMachine/MixTypes/Type_VOL.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_ECHO.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_FILTER.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_OCS_FILTER.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_PHASER.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_TRANCE.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_DISTORTION.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_FLANGER.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_PANNER.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_ROLL.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_FX_ROBOT.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MixTypes/Type_VOL.cpp
 
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/ManualMix/ManualMix.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/audioRender/ManualMix/MusicControlPanel.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/MainObjects/audioPlayer/audioPlayer.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/MainObjects/audioPlayer/audioCallbacks.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/ManualMix/ManualMix.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/ManualMix/MusicControlPanel.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/MainObjects/audioPlayer/audioPlayer.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/MainObjects/audioPlayer/audioCallbacks.cpp
 
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/interface/PDJE_interface.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/interface/PDJE_polyglot_wraps.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/interface/PDJE_interface.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/interface/PDJE_polyglot_wraps.cpp
 
 )
--- cmakes/src/DBsrc.cmake
+++ cmakes/src/DBsrc.cmake
@@ -1,17 +1,17 @@
 set(dbSource
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/musicDB.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/trackDB.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/dbRoot.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/dbState.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/musicDB.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/trackDB.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/dbRoot.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/dbState.cpp
 
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/Capnp/Translators/MixTranslator/Bpm.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/Capnp/Translators/MixTranslator/Mix.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/Capnp/Translators/MixTranslator/MixTranslator.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/Capnp/Translators/MusicTranslator/MusicTranslator.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/Capnp/Translators/NoteTranslator/NoteTranslator.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/MixTranslator/Bpm.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/MixTranslator/Mix.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/MixTranslator/MixTranslator.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/MusicTranslator/MusicTranslator.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/NoteTranslator/NoteTranslator.cpp
 
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/Capnp/Translators/FrameCalc.cpp
-${CMAKE_CURRENT_SOURCE_DIR}/core_include/db/fileNameSanitizer.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/FrameCalc.cpp
+${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/fileNameSanitizer.cpp
 
 ${CMAKE_CURRENT_SOURCE_DIR}/third_party/Capnp/MixBinary.capnp.c++
 ${CMAKE_CURRENT_SOURCE_DIR}/third_party/Capnp/MusicBinary.capnp.c++
