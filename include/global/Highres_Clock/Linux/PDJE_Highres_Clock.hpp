07f72f6e9e39ad50f3ab70af934eb7741644afb0
RROP <kimjiho1121@naver.com>
2025-10-11 16:59:30 +0900

    implemented

--- include/global/Highres_Clock/Linux/PDJE_Highres_Clock.hpp
+++ include/global/Highres_Clock/Linux/PDJE_Highres_Clock.hpp
@@ -1,5 +1,19 @@
 #pragma once
+#include <cstdint>
+#include <time.h>
 namespace PDJE_HIGHRES_CLOCK {
 
-class CLOCK {};
+class CLOCK {
+  private:
+    struct timespec ts;
+
+  public:
+    uint64_t
+    Get_MicroSecond()
+    {
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+
+        return (uint64_t)ts.tv_sec * 1000000ull + ts.tv_nsec / 1000;
+    }
+};
 }; // namespace PDJE_HIGHRES_CLOCK
