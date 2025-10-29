ab8236db59b56295bc94b6d62564520d2127313f
RROP <kimjiho1121@naver.com>
2025-10-29 17:42:06 +0900

    fix build flags

--- cmakes/includeDIRS.cmake
+++ cmakes/includeDIRS.cmake
@@ -1,64 +1,64 @@
 
 set(PDJE_INCLUDE_CORE
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/core
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/db
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/EFFECT
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/FAUST_VALS
+    ${PDJE_INCLUDE_ROOT}/include/core
+  ${PDJE_INCLUDE_ROOT}/include/core/db
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/MixMachine
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/MixMachine/EFFECT
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/MixMachine/FAUST_VALS
   
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MUSIC_CTR
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/MixMachine/MUSIC_CTR
   
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/MiniaudioObjects
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/MixMachine/MiniaudioObjects
   
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/MixMachine/INTERPOLATOR
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/MixMachine/INTERPOLATOR
 
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/ManualMix
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/audioRender/ManualMix/ManualFausts
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/CapnpBinary
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/MixTranslator
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/MusicTranslator
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/db/Capnp/Translators/NoteTranslator
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/ManualMix
+  ${PDJE_INCLUDE_ROOT}/include/core/audioRender/ManualMix/ManualFausts
+  ${PDJE_INCLUDE_ROOT}/include/core/db/Capnp/CapnpBinary
+  ${PDJE_INCLUDE_ROOT}/include/core/db/Capnp/Translators
+  ${PDJE_INCLUDE_ROOT}/include/core/db/Capnp/Translators/MixTranslator
+  ${PDJE_INCLUDE_ROOT}/include/core/db/Capnp/Translators/MusicTranslator
+  ${PDJE_INCLUDE_ROOT}/include/core/db/Capnp/Translators/NoteTranslator
 
 	${miniaudio_SOURCE_DIR}/extras/miniaudio_split
   ${SQLite3_INCLUDE_DIR}
   ${highway_INCLUDE_DIR}
 
-	${CMAKE_CURRENT_SOURCE_DIR}/third_party/SoundTouch/soundtouch_include
-	${CMAKE_CURRENT_SOURCE_DIR}/third_party/Faust/compiled
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/Faust/manual_compiled
+	${PDJE_INCLUDE_ROOT}/third_party/SoundTouch/soundtouch_include
+	${PDJE_INCLUDE_ROOT}/third_party/Faust/compiled
+  ${PDJE_INCLUDE_ROOT}/third_party/Faust/manual_compiled
   
 
   
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/MainObjects
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/MainObjects/audioPlayer
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/MainObjects/editorObject
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/MainObjects/tempDBObject
+  ${PDJE_INCLUDE_ROOT}/include/core/MainObjects
+  ${PDJE_INCLUDE_ROOT}/include/core/MainObjects/audioPlayer
+  ${PDJE_INCLUDE_ROOT}/include/core/MainObjects/editorObject
+  ${PDJE_INCLUDE_ROOT}/include/core/MainObjects/tempDBObject
   
 
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/interface
+  ${PDJE_INCLUDE_ROOT}/include/core/interface
 
 
 
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/edit
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/GitWrap
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/GitWrap/Branch
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/GitWrap/Commit
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/GitWrap/Add
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/GitWrap/Blame
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/GitWrap/Diff
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/GitWrap/Log
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/pdjeLinter
+  ${PDJE_INCLUDE_ROOT}/include/core/editor
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/edit
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/GitWrap
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/GitWrap/Branch
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/GitWrap/Commit
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/GitWrap/Add
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/GitWrap/Blame
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/GitWrap/Diff
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/GitWrap/Log
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/pdjeLinter
   
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/featureWrapper/JSONWrap
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/objects
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/core/editor/renderer
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/featureWrapper/JSONWrap
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/objects
+  ${PDJE_INCLUDE_ROOT}/include/core/editor/renderer
   
   ${CAPNPC_OUTPUT_DIR}/third_party/Capnp
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/input
+  ${PDJE_INCLUDE_ROOT}/include/input
 
   ${nlohmann_json_SOURCE_DIR}/include
   ${sql_amalgam_SOURCE_DIR}
@@ -66,47 +66,47 @@ set(PDJE_INCLUDE_CORE
   )
 
 set(PDJE_INCLUDE_JUDGE
-${CMAKE_CURRENT_SOURCE_DIR}/include/judge
-${CMAKE_CURRENT_SOURCE_DIR}/include/judge/NoteOBJ
-${CMAKE_CURRENT_SOURCE_DIR}/include/judge/Init
-${CMAKE_CURRENT_SOURCE_DIR}/include/judge/Loop
-${CMAKE_CURRENT_SOURCE_DIR}/include/global/Process
+${PDJE_INCLUDE_ROOT}/include/judge
+${PDJE_INCLUDE_ROOT}/include/judge/NoteOBJ
+${PDJE_INCLUDE_ROOT}/include/judge/Init
+${PDJE_INCLUDE_ROOT}/include/judge/Loop
+${PDJE_INCLUDE_ROOT}/include/global/Process
 ${CAPNPC_OUTPUT_DIR}/third_party/Capnp
 )
 
 set(PDJE_INCLUDE_INPUT
-${CMAKE_CURRENT_SOURCE_DIR}/include/input
-${CMAKE_CURRENT_SOURCE_DIR}/include/input/midi
-${CMAKE_CURRENT_SOURCE_DIR}/include/global/Process
-${CMAKE_CURRENT_SOURCE_DIR}/include/global/Process/utils
+${PDJE_INCLUDE_ROOT}/include/input
+${PDJE_INCLUDE_ROOT}/include/input/midi
+${PDJE_INCLUDE_ROOT}/include/global/Process
+${PDJE_INCLUDE_ROOT}/include/global/Process/utils
 
 )
 
 set(PDJE_INCLUDE_GLOBAL
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/global
-  ${CMAKE_CURRENT_SOURCE_DIR}/include/global/DataLines
+  ${PDJE_INCLUDE_ROOT}/include/global
+  ${PDJE_INCLUDE_ROOT}/include/global/DataLines
 )
 if(WIN32)
   list(APPEND PDJE_INCLUDE_GLOBAL
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/global/Highres_Clock/Windows
+    ${PDJE_INCLUDE_ROOT}/include/global/Highres_Clock/Windows
   )
   list(APPEND PDJE_INCLUDE_INPUT
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/input/windows
+    ${PDJE_INCLUDE_ROOT}/include/input/windows
   )
 elseif(APPLE)
   list(APPEND PDJE_INCLUDE_GLOBAL
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/global/Highres_Clock/Mac
+    ${PDJE_INCLUDE_ROOT}/include/global/Highres_Clock/Mac
   )
-  list(APPEND PDJE_INCLUDE_INPUT ${CMAKE_CURRENT_SOURCE_DIR}/include/input/apple)
+  list(APPEND PDJE_INCLUDE_INPUT ${PDJE_INCLUDE_ROOT}/include/input/apple)
 else()
   list(APPEND PDJE_INCLUDE_GLOBAL
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/global/Highres_Clock/Linux
+    ${PDJE_INCLUDE_ROOT}/include/global/Highres_Clock/Linux
   )
   list(APPEND PDJE_INCLUDE_INPUT
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/input/linux 
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/input/linux/RT
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/input/linux/common
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/input/linux/socket
+    ${PDJE_INCLUDE_ROOT}/include/input/linux 
+    ${PDJE_INCLUDE_ROOT}/include/input/linux/RT
+    ${PDJE_INCLUDE_ROOT}/include/input/linux/common
+    ${PDJE_INCLUDE_ROOT}/include/input/linux/socket
     
     )
 endif()
